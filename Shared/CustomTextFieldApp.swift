474831692739ef67f225f35f9b96ca9ce0e04663
paulerkab <52297604+paulerkab@users.noreply.github.com>
2021-05-06 20:37:17 +0300

    Initial Commit

---
+++ Shared/CustomTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldApp.swift
+//  Shared
+//
+//  Created by Ivan Kvasov on 06.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
